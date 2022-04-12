f7610ed1d94e473a0087cd0d2d1ec076c258cfb1
Long Tran <phuclongtranphan112@gmail.com>
2022-04-12 21:01:12 +0700

    added window.web3 = new Web3(provider)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-// import Web3 from 'web3';
+import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider';
-// import KryptoBird from '../abis/Kryptobird.json';
+import KryptoBird from '../abis/Kryptobird.json';
 
 class App extends Component{
 
@@ -12,26 +12,41 @@ class App extends Component{
  
     async loadWeb3() {
         const provider = await detectEthereumProvider();
+        
  
         if(provider) {
             console.log('It is working');
+            // Create web3 object
+            window.web3 = new Web3(provider);
         } else {
             console.log('No Ethereum Wallet Detected');
         };
     };
  
     async loadBlockchainData() {
+        const web3 = window.web3
         const accounts = await window.ethereum.request( {method: 'eth_requestAccounts'} );
-        this.setState({
-            acccount: accounts
-        })
-        console.log(this.state.account);
+        this.setState({account: accounts});
+        // console.log(this.state.account);
+        const networkId = await web3.eth.net.getId()
+        const networkData = KryptoBird.networks[networkId]
+        if(networkData){
+            const abi = KryptoBird.abi;
+            const address = networkData.address; 
+            const contract = new web3.eth.Contract(abi, address)
+    
+            this.setState({contract})
+            console.log(this.state.contract)
+
+
+        }
     };
 
     constructor(props){
         super(props);
         this.state = {
-            account: '2',
+            account: '',
+            contract: null
         }
 
     }
@@ -39,8 +54,20 @@ class App extends Component{
     render(){
         return(
             <div>
-                <h1>NFT Marketplace</h1>
+                <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
+                    <div className='navbar-brand col-sm-3 col-md-3 mr-0' style = {{color: 'white'}}>
+                        Krypto Birdz NFTs (Non Fungible Tokens)
+                    </div>
+                    <ul className='navbar-nav px-3'> 
+                        <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
+                            <small className='text-white'>
+                                {this.state.account}
+                            </small>
+                        </li>
+                    </ul>
+                </nav>
             </div>
+            
         )
     }
 
